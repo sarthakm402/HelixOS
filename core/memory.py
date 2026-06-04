830e1faf2c8af30d97672f4bca9b79694b7fe40c
sarthak mohapatra <sarthakm402@gmail.com>
2026-06-04 16:54:51 +0000

    added histroy in memory for now to chat

--- core/memory.py
+++ core/memory.py
@@ -1,8 +1,6 @@
 import json
 all_in_memory_history=[]
-def add_usrmsg_to_history(message:dict):
-    all_in_memory_history.append(message)
-def add_agtmsg_to_history(message:dict):
+def add_message(message: dict):
     all_in_memory_history.append(message)
 def get_all_history():
     return all_in_memory_history
--- frontend/terminal.py
+++ frontend/terminal.py
@@ -1,14 +1,21 @@
+import json
 from core.chat import ask
-print("""
-==================================
-      AI CYBERDECK v0.1
-==================================
-""")
+from core.memory import add_message
 while True:
     user_input = input("user> ")
     if user_input == "wq":
         break
+    add_message({
+        "role": "user",
+        "content": user_input
+    })
     print("Helix> ", end="")
+    assistant_response = ""
     for token in ask(user_input):
         print(token, end="", flush=True)
+        assistant_response += token
     print()
+    add_message({
+        "role": "assistant",
+        "content": assistant_response
+    })
