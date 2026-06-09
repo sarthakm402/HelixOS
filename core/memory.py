28205dbce3a560b0babb39ac16158ced94ce659f
sarthak mohapatra <sarthakm402@gmail.com>
2026-06-09 16:41:44 +0000

    added persisstance knowlenge when if we write/note it remebres message but have to add it to terminal,chat for llm to take,handle commands

--- core/memory.py
+++ core/memory.py
@@ -1,7 +1,19 @@
 import json
+import os
+
 all_in_memory_history=[]
-def add_message(message: dict):
+NOTES_FILE = "/home/sarthak/ml_code_new/HelixOS/notes.json"
+def add_message(message: dict,note=False):
     all_in_memory_history.append(message)
+    if note:
+        if os.path.exists(NOTES_FILE):
+            with open(NOTES_FILE, "r") as f:
+                notes = json.load(f)
+        else:
+            notes = []
+        notes.append(message)
+        with open(NOTES_FILE, "w") as f:
+            json.dump(notes, f, indent=2)
 def get_all_history():
     return all_in_memory_history
 def clear_all_history():
