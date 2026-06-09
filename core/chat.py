18005d8e6343dbca066a4feeaa7b3d8868d9791f
sarthak mohapatra <sarthakm402@gmail.com>
2026-06-09 16:02:29 +0000

    added memeory usaeg in convo

--- core/chat.py
+++ core/chat.py
@@ -1,4 +1,18 @@
 from services.llm import chat_with_llm
-def ask(prompt):
+from core.memory import get_all_history
+def ask(user_query):
+    history = get_all_history()
+    conversation = "\n".join(
+    f"{msg['role']}: {msg['content']}"
+    for msg in history[-10:]
+)
+    prompt = f"""
+Previous Conversation:
+
+{conversation}
+
+Current User Message:
+{user_query}
+"""
     for token in chat_with_llm(prompt):
         yield token
